bed671b3f3fd175b167381b8a23c672b9108f363
s1211533 <54788460+s1211533@users.noreply.github.com>
2020-01-02 16:28:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const ExifImage = require('exif').ExifImage;
 
 app.set('view engine','ejs');
 
-
 app.get('/', (req,res) => {
 	res.redirect('/filetoupload');
 });
@@ -24,44 +23,45 @@ app.post('/filetoupload' , (req,res) => {
 		let make = null;
 		let model = null;
 		let createTime = null;
-		let latitude0 = null;
-		let latitude1 = null;
-		let latitude2 = null;
-		let longitude0 = null;
-		let longitude1 = null;
-		let longitude2 = null;
-		let latitude = null;
-		let longitude = null;
+		let location = null;
+		let dla = null;
+		let mla = null;
+		let sla = null;
+		let dlo = null;
+		let mlo = null;
+		let slo = null;
+		let lat = null;
+		let lon = null;;
 		title = fields.title;
         	description = fields.description;
+		console.log("1");
 		fs.readFile(files.filetoupload.path, (err,data) => {
 			image = new Buffer.from(data).toString('base64');
 			try {
     				new ExifImage({ image : files.filetoupload.path }, function (error, exifData) {
         				if (error)
             					console.log('Error: '+error.message);
-        				else
+        				else{
 						make = exifData.image.Make; 
 						model = exifData.image.Model;
 						createTime = exifData.exif.CreateDate;
-					        latitude0 = exifData.gps.GPSLatitude[0];
-						latitude1 = exifData.gps.GPSLatitude[1];
-						latitude2 = exifData.gps.GPSLatitude[2];
-						latitude = latitude0 + latitude1/60 + latitude2/3600;
-						longitude0 = exifData.gps.GPSLongitude[0];
-						longitude1 = exifData.gps.GPSLongitude[1];
-						longitude2 = exifData.gps.GPSLongitude[2];
-						longitude = longitude0 + longitude1/60 + longitude2/3600;
-						res.status(200).render('display', {t :title, d :description, i: image, ma: make, mo: model, c: createTime});	
-app.get('/map', (req,res) => {
-res.status(200).render('map' ,{la: latitude, lo: longitude});
-}); 
-				      }
-				});
+						dla = exifData.gps.GPSLatitude[0];
+						mla = exifData.gps.GPSLatitude[1];
+						sla = exifData.gps.GPSLatitude[2];
+						lat = dla + mla/60 + sla/3600;
+						dlo = exifData.gps.GPSLongitude[0];
+						mlo = exifData.gps.GPSLongitude[1];
+						slo = exifData.gps.GPSLongitude[2];
+						lon = dlo + mlo/60 + slo/3600;
+						res.status(200).render('display', {t :title, d :description, i: image, ma: make, mo: model, c: createTime});
+						app.get('/map', (req,res) => {
+							res.status(200).render('map' ,{la: lat, lo: lon});
+						}); 
+					}
+    				});
 			} catch (error) {
    				 console.log('Error: ' + error.message);
 			}
-			
 		});
 	});
 });
